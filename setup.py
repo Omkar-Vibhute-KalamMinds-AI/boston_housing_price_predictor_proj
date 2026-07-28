d6d240fa86086472acb0368e57d0be3f64329c4e
[Omkar Vibhute] <[vihuteomkar64@gmail.com]>
2026-07-28 13:37:55 +0530

    commit_2

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup 
+
+HYPE_E_DOT = '-e .' 
+
+def get_requirements(file_path:str) -> list[str]: 
+    """ This function returns a list of project requirements. 
+    """
+    requirements = [] 
+    
+    with open(file_path) as file_obj: 
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n','') for req in requirements] 
+        
+        if HYPE_E_DOT in file_path: 
+            requirements.remove() 
+        
+    return requirements 
+    
+
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'KalamsVisionAILabs',
+    autor_email = 'kalamsvisionlabs@gmail.com',  
+    package = find_packages(), 
+    install_requires= get_requirements('requirements.txt')
+    )
+
